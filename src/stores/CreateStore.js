a4096cbe6993eee7907eb85b33f4350bc4310d58
AposVinc <apos.vincenzo@gmail.com>
2020-09-02 11:23:03 +0200

    Update CreateStore.js

--- src/stores/CreateStore.js
+++ src/stores/CreateStore.js
@@ -1,8 +1,10 @@
-import {createStore, compose} from 'redux';
+import {createStore, compose, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 
 export default (reducers) => {
 
-  const enhancers = [];
+  const middleware = [thunk];
+  const enhancers = [applyMiddleware(...middleware)];
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const store = createStore(reducers, composeEnhancers(...enhancers));
 
