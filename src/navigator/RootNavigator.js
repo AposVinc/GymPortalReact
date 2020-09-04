f8e4823d955c92991bbd9775f47cfe29d29f7bb2
AposVinc <apos.vincenzo@gmail.com>
2020-09-05 00:20:18 +0200

    update RootNavigator for login

--- src/navigator/RootNavigator.js
+++ src/navigator/RootNavigator.js
@@ -3,17 +3,31 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import GymListPage from '../pages/GymListPage';
 import GymPage from '../pages/GymPage';
+import LoginPage from '../pages/LoginPage';
+import SignUpPage from '../pages/SingUpPage';
+import {sAppLogged} from '../reducers/AppReducer';
+import {useSelector} from 'react-redux';
 
 const RootStack = createStackNavigator();
 
 export default function() {
+  const logged = useSelector(sAppLogged);
+
   return (
       <NavigationContainer>
         <RootStack.Navigator>
-
-          <RootStack.Screen name={'Gym List'} component={GymListPage}/>
-          <RootStack.Screen name={'Gym'} component={GymPage}/>
-
+          {!logged && (
+              <>
+                <RootStack.Screen name={'SignIn'} component={LoginPage}/>
+                <RootStack.Screen name={'SignUp'} component={SignUpPage}/>
+              </>
+          )}
+          {logged && (
+              <>
+                <RootStack.Screen name={'Gym List'} component={GymListPage}/>
+                <RootStack.Screen name={'Gym'} component={GymPage}/>
+              </>
+          )}
         </RootStack.Navigator>
       </NavigationContainer>
   );
