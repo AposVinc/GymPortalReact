82dea05dc3d552dd1a3ca0bfa5f004b747593758
fons94 <djpoldowlf@gmail.com>
2020-09-09 20:23:21 +0200

    restituisce l'id della palestra ma non i corsi della palestra selezionata

--- src/actions/CourseAction.js
+++ src/actions/CourseAction.js
@@ -2,9 +2,9 @@ import {COURSE_FETCH} from '../stores/ActionType';
 import * as API from '../api'
 
 
-export function courseFetch(){
+export function courseFetch({idGym}){
   return {
     type: COURSE_FETCH,
-    payload: API.getAllCourses().then(r => ({courses: r}))
+    payload: API.getAllCourses(idGym).then(r => ({courses: r}))
   };
 }
