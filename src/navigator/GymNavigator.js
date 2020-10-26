4c037c1025ca957ea56d282a6e63c5e933afc05e
AposVinc <apos.vincenzo@gmail.com>
2020-10-26 19:36:18 +0100

    Update GymNavigator.js

--- src/navigator/GymNavigator.js
+++ src/navigator/GymNavigator.js
@@ -2,6 +2,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import GymListPage from '../pages/GymListPage';
 import React from 'react';
 import GymPage from '../pages/GymPage';
+import CoursePage from '../pages/CoursePage';
 
 const Stack = createStackNavigator();
 
@@ -11,6 +12,7 @@ export default function() {
 
         <Stack.Screen name={'Gym List'} component={GymListPage} />
         <Stack.Screen name={'Gym'} component={GymPage}/>
+        <Stack.Screen name={'Course'} component={CoursePage}/>
 
       </Stack.Navigator>
   )
