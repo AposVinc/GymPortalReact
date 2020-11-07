58732c18637567ae3c0be768e58d29bde960eb0e
AposVinc <apos.vincenzo@gmail.com>
2020-11-07 13:58:05 +0100

    Update FeedbackItem.js

--- src/pages/gyms/partial/FeedbackItem.js
+++ src/pages/gyms/partial/FeedbackItem.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, Text, View} from 'react-native';
 import {CardItem} from '../../../components';
+import {Rating} from 'react-native-elements';
+import {getUser} from '../../../api';
 
 export default function({feedback}) {
+  const [user, setUser] = useState({user: null});
+
+  useEffect( () => {
+    getUser(feedback.user).
+        then( data => {
+          data.lastname = data.lastname.substr(0,1);
+          setUser(data);
+        });
+  }, []);
+
+  if (user === null) {
+    return (
+        <View
+            style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <ActivityIndicator size={'large'} color={'green'} />
+        </View>
+    );
+  }
 
   return (
-      <CardItem>
-        <View style={styles.container}>
-          <Text>Id: {feedback.id}</Text>
-        </View>
+      <CardItem style={styles.container}>
+        <CardItem style={styles.row.container}>
+
+          <Text style={[styles.row.column, {fontWeight: 'bold'}]}>
+            {user.name} {user.lastname}.
+          </Text>
+          <Rating style={[styles.row.column, {flexDirection: "column"}]}
+                  imageSize={20}
+                  readonly
+                  startingValue={feedback.rating} />
+
+        </CardItem>
+
+        <Text>{feedback.feed}</Text>
+
       </CardItem>
   );
 
@@ -16,21 +47,14 @@ export default function({feedback}) {
 
 const styles = {
   container: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    marginBottom: 24,
   },
-  containerText: {
-    justifyContent: 'space-around',
-  },
-  thumb: {
-    height: 50,
-    width: 50,
-  },
-  image: {
-    height: 200,
-    width: 200,
-  },
-  button: {
-    width: 100,
-  },
+  row:{
+    container:{
+      flexDirection: "row", marginBottom: 10
+    },
+    column:{
+      flex: 1, paddingHorizontal: 0
+    }
+  }
 };
