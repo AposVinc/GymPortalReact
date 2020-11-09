d55c2ea4a431272bb2c10a23681e88e2428bde18
AposVinc <apos.vincenzo@gmail.com>
2020-11-09 18:00:09 +0100

    feedback reducer

--- src/actions/FeedbackActions.js
+++ src/actions/FeedbackActions.js
@@ -1,8 +1,12 @@
 import {
+  FAVORITE_COURSE_FETCH,
+  FEEDBACK_ADD,
+  FEEDBACK_CHANGE_FEED, FEEDBACK_CHANGE_RATING, FEEDBACK_RESET, FEEDBACK_UPDATE,
   FEEDBACKS_COURSE_FETCH,
-  FEEDBACKS_GYM_FETCH,
+  FEEDBACKS_GYM_FETCH, USER_FORM_CHANGE_VALUE,
 } from '../stores/ActionType';
 import * as API from '../api';
+import {sUserProps, sUserToken} from '../reducers/UserReducer';
 
 
 export function feedbacksGymFetch(idGym){
@@ -22,4 +26,25 @@ export function feedbacksCourseFetch(idGym, idCourse){
     });
   };
 }
+
+
+
+
+export const feedbackChangeFeed = function(value) {
+  return {
+    type: FEEDBACK_CHANGE_FEED,
+    payload: {value}
+  };
+};
+export const feedbackChangeRating = function(value) {
+  return {
+    type: FEEDBACK_CHANGE_RATING,
+    payload: {value}
+  };
+};
+export const feedbackReset = function() {
+  return {
+    type: FEEDBACK_RESET,
+  };
+};
 
--- src/reducers/FeedbackReducer.js
+++ src/reducers/FeedbackReducer.js
@@ -1,14 +1,20 @@
 import {
+  FEEDBACK_CHANGE_FEED, FEEDBACK_CHANGE_RATING, FEEDBACK_RESET,
   FEEDBACKS_COURSE_FETCH, FEEDBACKS_GYM_FETCH, USER_LOGGED_OUT,
 } from '../stores/ActionType';
 
 const INITIAL_STATE = {
+  feedback:{
+    rating:5,
+    feed:"",
+  },
   feedbacksGym: [],
   feedbacksCourse: [],
   loading: false,
 };
 
 const sFeedback = (state) => state.feedback;
+export const sFeedbacksCurrentFeedback = state => sFeedback(state).feedback;
 export const sFeedbacksGym = state => sFeedback(state).feedbacksGym;
 export const sFeedbacksCourse = state => sFeedback(state).feedbacksCourse;
 export const sFeedbackLoading = state => sFeedback(state).loading;
@@ -50,6 +56,30 @@ export default function(state = INITIAL_STATE, action) {
         loading: false,
       };
 
+    case FEEDBACK_CHANGE_FEED:
+      return {
+        ...state,
+        feedback:{
+          ...state.feedback,
+          feed: action.payload.value,
+        }
+      };
+    case FEEDBACK_CHANGE_RATING:
+      return {
+        ...state,
+        feedback:{
+          ...state.feedback,
+          rating: action.payload.value,
+        }
+      };
+    case FEEDBACK_RESET:
+      return {
+        ...state,
+        feedback: {
+          rating: 5,
+          feed: ""
+        }
+      };
 
     case USER_LOGGED_OUT:
       return INITIAL_STATE;
