63ac1e67a360d16e79e4018393895fa32972be6a
AposVinc <apos.vincenzo@gmail.com>
2020-11-10 12:24:28 +0100

    add addFeedbackCoursePage to navigator

--- src/navigator/partial/GymNavigator.js
+++ src/navigator/partial/GymNavigator.js
@@ -6,6 +6,7 @@ import CoursePage from '../../pages/CoursePage';
 import React from 'react';
 import {MenuButtom} from '../../components';
 import AddFeedbackGymPage from '../../pages/AddFeedbackGymPage';
+import AddFeedbackCoursePage from '../../pages/AddFeedbackCoursePage';
 
 const StackNavigator = createStackNavigator();
 export default function GymNavigator({navigation}) {
@@ -24,7 +25,7 @@ export default function GymNavigator({navigation}) {
         <StackNavigator.Screen name={'Add Feedback Gym'} component={AddFeedbackGymPage}/>
         <StackNavigator.Screen name={'Courses List'} component={CourseListPage}/>
         <StackNavigator.Screen name={'Course'} component={CoursePage}/>
-        <StackNavigator.Screen name={'Add Feedback Course'} component={AddFeedbackGymPage}/>
+        <StackNavigator.Screen name={'Add Feedback Course'} component={AddFeedbackCoursePage}/>
       </StackNavigator.Navigator>
   )
 }
