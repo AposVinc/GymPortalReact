638722fa7a3e3be5688ec923f9f2c37540db8baa
AposVinc <apos.vincenzo@gmail.com>
2020-11-11 20:47:16 +0100

    api for search

--- src/api/CourseApi.js
+++ src/api/CourseApi.js
@@ -1,8 +1,7 @@
 import axios from './axiosConfig';
 
 const URL_GYMS = 'gyms/';
-const URL_COURSES = '/courses/';
-const URL_FEEDBACKS = '/feedbacks';
+const URL_COURSES = '/courses';
 
 export const getAllCourses = function (idGym) {
   return axios.get(URL_GYMS + idGym + URL_COURSES).
@@ -12,8 +11,16 @@ export const getAllCourses = function (idGym) {
       });
 };
 
+export const getCoursesByName = function (idGym, name) {
+  return axios.get(URL_GYMS + idGym + URL_COURSES + "?name=" + name).
+      then(response => response.data).
+      catch(error => {
+        throw error;
+      });
+};
+
 export const getCourse = function (idGym, idCourse) {
-  return axios.get(URL_GYMS + idGym + URL_COURSES + idCourse).
+  return axios.get(URL_GYMS + idGym + URL_COURSES + "/" + idCourse).
       then(response => response.data).
       catch(error => {
         throw error;
--- src/api/GymApi.js
+++ src/api/GymApi.js
@@ -1,6 +1,6 @@
 import axios from './axiosConfig';
 
-const URL_GYMS = 'gyms/';
+const URL_GYMS = 'gyms';
 
 export const getAllGyms = function () {
   return axios.get(URL_GYMS).
@@ -10,8 +10,24 @@ export const getAllGyms = function () {
       });
 };
 
+export const getGymsByName = function (name) {
+  return axios.get(URL_GYMS + "?name=" + name).
+      then(response => response.data).
+      catch(error => {
+        throw error;
+      });
+};
+
+export const getGymsByRegion = function (region) {
+  return axios.get(URL_GYMS + "?region=" + region).
+      then(response => response.data).
+      catch(error => {
+        throw error;
+      });
+};
+
 export const getGym = function (idGym) {
-  return axios.get(URL_GYMS + idGym).
+  return axios.get(URL_GYMS + "/" + idGym).
       then(response => response.data).
       catch(error => {
         throw error;
